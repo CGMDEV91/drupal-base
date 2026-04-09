5b2ab31894ef7d92ae2739c3acad2e50e11acf83
Carlos GONZALEZ MUJICA <Carlos.GONZALEZMUJICA@netcompany.com>
2026-04-09 07:49:31 +0200

    configure config import filter

--- web/modules/custom/stepuptours_config_filter/src/Plugin/ConfigFilter/StepUpToursConfigFilter.php
+++ web/modules/custom/stepuptours_config_filter/src/Plugin/ConfigFilter/StepUpToursConfigFilter.php
@@ -15,41 +15,54 @@ class StepUpToursConfigFilter extends ConfigFilterBase {
 
   /**
    * {@inheritdoc}
-   * Se ejecuta cuando Drupal LEE config de bbdd para usarla en runtime.
-   * Inyecta los valores de env vars en memoria.
    */
   public function filterRead($name, $data) {
-    return $this->injectEnvVars($name, $data);
-  }
-
-  /**
-   * {@inheritdoc}
-   * Se ejecuta cuando el cim va a ESCRIBIR en bbdd.
-   * Inyecta los valores de env vars para que queden persistidos.
-   */
-  public function filterWrite($name, array $data) {
-    return $this->injectEnvVars($name, $data);
-  }
-
-  /**
-   * {@inheritdoc}
-   * Se ejecuta cuando Drupal LEE del sync storage (YML) durante el cim.
-   * Inyecta env vars en lo que viene del YML antes de comparar/escribir.
-   */
-  public function filterReadPersist($name, $data) {
-    return $this->injectEnvVars($name, $data);
-  }
-
-  /**
-   * Inyecta valores de env vars en la config.
-   */
-  protected function injectEnvVars($name, $data) {
-    if ($name === 'stepuptours_api.payment' && is_array($data)) {
-      $data['stripe_secret_key']      = getenv('STEPUP_TOURS_STRIPE_SECRET_KEY') ?: '';
-      $data['stripe_publishable_key'] = getenv('STEPUP_TOURS_STRIPE_PUBLISHABLE_KEY') ?: '';
-      $data['stripe_webhook_secret']  = getenv('STEPUP_TOURS_STRIPE_WEBHOOK_KEY') ?: '';
+    $map = $this->getMap();
+    if (isset($map[$name])) {
+      $this->iterateMap($name, $data, $map[$name]);
     }
     return $data;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function filterWrite($name, array $data): array {
+    $map = $this->getMap();
+    if (isset($map[$name])) {
+      $this->iterateMap($name, $data, $map[$name], 'export');
+    }
+    return $data;
+  }
+
+  /**
+   * Returns map of config fields with env variables.
+   */
+  protected function getMap(): array {
+    return [
+      'stepuptours_api.payment' => [
+        'stripe_secret_key'            => 'STEPUP_TOURS_STRIPE_SECRET_KEY',
+        'stripe_publishable_key'       => 'STEPUP_TOURS_STRIPE_PUBLISHABLE_KEY',
+        'stripe_webhook_secret'        => 'STEPUP_TOURS_STRIPE_WEBHOOK_KEY',
+      ],
+    ];
+  }
+
+  /**
+   * Iterates and set the correct values on config import and export.
+   */
+  protected function iterateMap($name, &$data, $mapPerName, $type = 'import'): void {
+    foreach ($mapPerName as $key => $value) {
+      if (is_array($value)) {
+        $this->iterateMap($name, $data[$key], $value, $type);
+      }
+      elseif (!(is_array($value))) {
+        $data[$key] = '';
+        if ($type === 'import') {
+          $data[$key] = getenv($value);
+        }
+      }
+    }
   }
 
 }
